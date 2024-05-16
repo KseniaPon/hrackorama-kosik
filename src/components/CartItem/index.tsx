d2c9bdf8ab55e397f5d5ca05f1f50789afaa1e3c
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-05-16 19:35:53 +0200

    1 uroven vyssi stav

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -2,16 +2,18 @@ import IProduct from "../../models/Product";
 import Amount from "../Amount";
 import CartProduct from "../CartProduct";
 import './style.css';
+import { useState } from "react";
 
 interface ICartItemProps {
   product: IProduct;
 }
 
 const CartItem: React.FC<ICartItemProps> = ({ product }) => {
+  const [count, setCount] = useState<number>(product.amount);
   return (
     <div className="cart-item">
       <CartProduct name={product.name} price={product.price} />
-      <Amount value={product.amount} />
+      <Amount count={count} onChange={setCount}/>
     </div>
   )
 };
